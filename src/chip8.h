d1d496672a39f0a759087f88cc9f9fe97926a4fc
piotrselak <piotr.selak.it@gmail.com>
2024-11-20 19:21:15 +0100

    Even more opcodes handlers

--- src/chip8.h
+++ src/chip8.h
@@ -38,25 +38,77 @@ public:
     void execute(const uint16_t opcode) {
         const uint8_t x = opcode >> 8 & 0xF;
         const uint8_t y = opcode >> 4 & 0xF;
+        const uint16_t nnn = opcode & 0xFFF;
         const uint8_t nn = opcode & 0xFF;
         const uint8_t n = opcode & 0xF;
 
         // TODO maybe tidy and move it somewhere else if needed
         switch (uint8_t first_nible = opcode >> 12) {
             case 0x0:
-                clear_screen();
+                if (nnn == 0x0EE) {
+                    pc = stack.top();
+                    stack.pop();
+                } else if (nnn == 0x0E0)
+                    clear_screen();
                 break;
             case 0x1:
                 pc = opcode & 0xFFF;
                 break;
+            case 0x2:
+                stack.push(pc);
+                pc = nnn;
+                break;
+            case 0x3:
+                if (v[x] == nn)
+                    i += 2;
+                break;
+            case 0x4:
+                if (v[x] != nn)
+                    i += 2;
+            case 0x5:
+                if (v[x] == v[y])
+                    i += 2;
             case 0x6:
                 v[x] = nn;
                 break;
             case 0x7:
                 v[x] += nn;
                 break;
+            case 0x8:
+                if (n == 0)
+                    v[x] = v[y];
+                else if (n == 1)
+                    v[x] |= v[y];
+                else if (n == 2)
+                    v[x] &= v[y];
+                else if (n == 3)
+                    v[x] ^= v[y];
+                else if (n == 4) {
+                    v[0xf] = v[x] > 255 - v[y] ? 1 : 0;
+                    v[x] += v[y];
+                } else if (n == 5) {
+                    v[0xf] = v[x] > v[y] ? 1 : 0;
+                    v[x] -= v[y];
+                } else if (n == 6) {
+                    v[0xf] = v[x] & 0x1;
+                    v[x] >>= 1;
+                } else if (n == 7) {
+                    v[0xf] = v[y] > v[x] ? 1 : 0;
+                    v[x] = v[y] - v[x];
+                } else if (n == 0xE) {
+                    v[0xf] = v[x] >> 7;
+                    v[x] <<= 1;
+                }
+                break;
+            case 0x9:
+                if (v[x] != v[y])
+                    pc += 2;
+                break;
             case 0xA:
                 i = opcode & 0xFFF;
+                break;
+            case 0xB:
+                pc = v[0] + nnn;
                 break;
             case 0xD:
                 // TODO FIX AND TIDY
